3c1d75e58c1a7fa424cf6e69fdf366c45631b107
DESARROLLOSNEA\jose.fernandez <jfernandez@desarrollosnea.com>
2019-11-16 15:31:07 -0300

    print neighborhood to console

--- src/main.go
+++ src/main.go
@@ -8,6 +8,10 @@ import (
 func main() {
 	fmt.Println("Game of life!")
 
+	var g = CreateNeighborhood(3, 3)
+
+	print(" Step 1 ", g)
+
 }
 
 func CreateNeighborhood(x, y int) [][]int {
@@ -29,10 +33,11 @@ func GetCell(neighborhood [][]int, x, y int) int {
 	return neighborhood[x][y]
 }
 
-func printSlice(s string, x [][]int) {
+func print(s string, x [][]int) {
 	for i := 0; i < len(x); i++ {
 		for j := 0; j < len(x[i]); j++ {
-			fmt.Printf("%d\n", x[i][j])
+			fmt.Printf("%d ", x[i][j])
 		}
+		fmt.Println("")
 	}
 }
